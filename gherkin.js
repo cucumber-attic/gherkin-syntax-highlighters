96326f016ac477f8dd612630df2fccf0c00f5595
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-10-03 19:27:06 +0100

    Simplify. Closes #2.

--- gherkin.js
+++ gherkin.js
@@ -3,27 +3,25 @@ Language: Gherkin
 */
 
 hljs.LANGUAGES.gherkin = function() {
-  var DOC_STRING = {
-    className: 'string',
-    begin: '"""', end: '"""',
-    relevance: 10
-  };
-
   return {
     defaultMode: {
-      keywords: {
-        'keyword': {
-          'Feature': 1, 'Scenario': 1, 'Scenario Outline': 1, 'Examples': 1, 'Given': 1, 'When': 1, 'Then': 1, 'And': 1, 'But': 1
-        }
-      },
-      illegal: '(</|->|\\?)',
       contains: [
+        {
+          className: 'keyword',
+          begin: '^\\s*(Feature|Scenario|Scenario Outline|Examples|Given |When |Then |And |But )',
+          relevance: 0
+        },
         hljs.HASH_COMMENT_MODE,
-        DOC_STRING, hljs.APOS_STRING_MODE, hljs.QUOTE_STRING_MODE,
+        {
+          className: 'string',
+          begin: '"""', end: '"""',
+          relevance: 10
+        },
+        hljs.APOS_STRING_MODE, 
+        hljs.QUOTE_STRING_MODE,
         hljs.C_NUMBER_MODE,
         {
-          className: 'decorator',
-          begin: '@', end: '$'
+          className: 'annotation', begin: '@[^@\r\n\t ]+'
         }
       ]
     }
