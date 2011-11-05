1592a58d9ebedf22bfa2321d1e4162481ae30ee8
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2011-11-05 01:11:49 +0000

    One file

--- ace/gherkin.js
+++ ace/gherkin.js
@@ -3,9 +3,48 @@ define('ace/mode/gherkin', function(require, exports, module) {
 var oop = require("pilot/oop");
 var TextMode = require("ace/mode/text").Mode;
 var Tokenizer = require("ace/tokenizer").Tokenizer;
-var GherkinHighlightRules = require("ace/mode/gherkin_highlight_rules").GherkinHighlightRules;
 var MatchingBraceOutdent = require("ace/mode/matching_brace_outdent").MatchingBraceOutdent;
 var Range = require("ace/range").Range;
+var TextHighlightRules = require("ace/mode/text_highlight_rules").TextHighlightRules;
+var GherkinHighlightRules = function()
+{
+    this.$rules = {
+        "start" : [
+            {
+                token : "comment",
+                regex : "^\\s*#.*$"
+            }, 
+            {
+                token : "comment.doc.tag",
+                regex : "@[^@\\\r\\\n\\\t ]+" 
+            },
+            {
+                token : "keyword.with_children",
+                regex : "^\\s*(?:Feature|Scenario|Scenario Outline|Examples):",
+            },
+            {
+                token : "keyword",
+                regex : "^\\s*(?:Given |When |Then |And |But )"
+            },
+            {
+                token : "string",           // multi line """ string start
+                regex : '^\\s*"{3}.*$',
+                next : "qqstring"
+            },
+        ],
+        "qqstring" : [ {
+                token : "string", // multi line """ string end
+                regex : '(?:[^\\\\]|\\\\.)*?"{3}',
+                next : "start"
+            }, {
+                token : "string",
+                regex : '.+'
+            }
+        ]
+    };
+};
+
+oop.inherits(GherkinHighlightRules, TextHighlightRules);
 
 var Mode = function()
 {
